aabc4f8a2230f12fb4f1d3b1aab13285e5e79d83
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2022-10-07 13:40:01 +0500

    eco

--- src/API/API.js
+++ src/API/API.js
@@ -1,10 +1,10 @@
 const API = [
-    { name : "Apple", price: "200" },
-    { name : "Mango", price : "400" },
-    { name : "Melon", price : "100" },
-    { name : "Banana", price : "500" },
-    { name : "Cheeku", price : "700" },
-    { name : "Grape", price : "100" }
+    { name : "Apple", price: 200 , quantity : 1 },
+    { name : "Mango", price : 400 , quantity : 1 },
+    { name : "Melon", price : 100 , quantity : 1 },
+    { name : "Banana", price : 500 , quantity : 1 },
+    { name : "Cheeku", price : 700 , quantity : 1 },
+    { name : "Grape", price : 100 , quantity : 1 }
 ]
 
 export default API;
--- src/App.js
+++ src/App.js
@@ -5,20 +5,67 @@ import API from './API/API';
 import { useState , useEffect } from 'react';
 
 function App() {
-  const [value, setValue] = useState([])
+  const [value, setValue] = useState([]);
+  const [cost, setCost] = useState();
+  const [stateUpdate , setStateUpdate] = useState()
+
+ 
+    // handle add
+const handleAdd = (res) => {
+  setValue([...value, res]);  
+}
   
-  // handle add to list
-  const handleAdd = (res) => {
-    setValue([...value, res]);
+  // handle remove
+  const handleRemove = (res) => {
+    const a = value.filter((item) => item?.name !== res?.name)      
+    setValue(a)
+  }  
+
+
+  // handle add quantity
+  const handleAddQuantity = (res) => {
+    const fIndex = value.findIndex((item) => item?.name === res?.name)
+    var a = value[fIndex].quantity += 1  
+    var price = res?.price * a  
+    totalCost(price)
+    setStateUpdate(!stateUpdate)
+    console.log("price ====>", price);
+  }
+
+
+  
+  // total cost
+  const totalCost = (price) => {
+    const a = value.reduce((accum, curr) => {
+    return accum += price
+    }, 0)
+
+    setCost(a)
   }
 
 
 
+  // handle decrease quantity
+  const handleRemoveQuantity = (res) => {
+    const fIndex = value.findIndex((item) => item?.name === res?.name)
+    setStateUpdate(!stateUpdate)
+    if(value[fIndex].quantity === 1) {
+      return false
+    } else {
+    var a = value[fIndex].quantity -= 1          
+    var price = res?.price * a  
+    totalCost(price)
+    setStateUpdate(!stateUpdate)     
+
+    }
+  }
+
+  useEffect(() => {
+   totalCost()
+  }, [value])
 
 
 
-  
-  console.log("value list---->", value)
   return (
     <>
       {/* <BrowserRouter>
@@ -31,9 +78,8 @@ function App() {
         return (
           <>
             <div style={{display : "flex", flexDirection: "row"}}>
-              <h2>{res?.name} {res?.price} </h2>
+              <h2>{res?.name} {res?.price} </h2> { value.some((item) => item?.name === res?.name) ? <button onClick={() => handleRemove(res)}>Remove</button> : <button onClick={() => { handleAdd(res)}}>Add To Cart</button> }   
                
-              <button onClick={() => { handleAdd(res) }}> Add To Cart </button>
             </div>
           </>
         )
@@ -41,19 +87,22 @@ function App() {
 
       <br />
       <br />
-      <br />
+      <br />  
       <br />
       <br />
       <h2>Cart List</h2>
     
-      {value.map((res) => {
+      {value.map((res, index) => {
         return (
           <>
-            <p>{res?.name}{res?.price}</p>  
+            <p style={{ display: "flex" }}>{res?.name} {res?.price}$ <button onClick={() => handleAddQuantity(res)}>+</button> <p>   {res?.quantity}</p> <button onClick={() => handleRemoveQuantity(res)}>-</button> </p>  
           </>
          )
-       }) } 
-
+      })} 
+      
+      <br /><br />
+      <h2>Total Cost</h2>
+      <p>{cost}$</p>
     </>
   );
 }
